594d3f063946abdf3431124583b7567edd093826
ipavanteja <pavankunchala54@gmail.com>
2024-09-24 21:58:41 +0530

    added reselect package to stope unwanted re-renders in category component

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,14 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+// Input selector to get categories array from state
+const selectCategories = (state) => state.categories.categories;
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
